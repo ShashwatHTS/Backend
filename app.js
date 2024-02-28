981b3678ccd5263b9b5dbfe4e1ce4b29a088af36
ShashwatHTS <shashwat.pal@htstechsolutions.com>
2024-02-28 17:35:18 +0530

    added a cores

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 
 var createError = require('http-errors');
 var express = require('express');
@@ -17,7 +18,7 @@ app.set('view engine', 'jade');
 
 app.use(
   cors({
-    origin: '*',
+    origin: 'https://backend-9h20.onrender.com',
     credentials: true,
   })
 );
@@ -33,12 +34,12 @@ app.use('/todo', todoRouter);
 app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
